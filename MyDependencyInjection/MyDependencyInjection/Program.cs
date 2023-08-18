2b46257d44fd889d82bc8d1cfba61ba2a4025a37
donatellony <donatellony4736@gmail.com>
2023-08-18 21:10:23 +0200

    Added some test cases to test out implemented features. Looks good :)

--- MyDependencyInjection/MyDependencyInjection/Program.cs
+++ MyDependencyInjection/MyDependencyInjection/Program.cs
@@ -1,17 +1,65 @@
 ﻿using MyDependencyInjectionConsoleApp.Services;
 using MyDependencyInjectionLibrary;
 
-var services = new ServiceCollection();
+// WARNING - USING MULTIPLE SERVICE PROVIDERS CAN BE DANGEROUS AND LEAD TO UNPREDICTABLE ERRORS!!!!!!
+// I'm using them ONLY to demonstrate different lifetimes
+{
+    Console.WriteLine("Everything as the transient");
+    var services = new ServiceCollection();
 
-services.AddSingleton<IConsoleWriter, ConsoleWriter>();
-services.AddTransient<IDateTimeProvider, DateTimeProvider>();
-services.AddSingleton<IGuidGenerator, GuidGenerator>();
-services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+    services.AddTransient<IGuidGenerator, GuidGenerator>();
+    services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+    services.AddTransient<IConsoleWriter, ConsoleWriter>();
 
 
-var serviceProvider = services.BuildServiceProvider();
+    var serviceProvider = services.BuildServiceProvider();
 
-var guidGenerator = serviceProvider.GetService<IGuidGenerator>();
-var guidGenerator2 = serviceProvider.GetService<IGuidGenerator>();
-Console.WriteLine(guidGenerator.GetGuidAndNowTicks());
-Console.WriteLine(guidGenerator2.GetGuidAndNowTicks());
+    var guidGenerator = serviceProvider.GetService<IGuidGenerator>();
+    var guidGenerator2 = serviceProvider.GetService<IGuidGenerator>();
+    Console.WriteLine(guidGenerator!.GetGuidAndNowTicks());
+    Console.WriteLine(guidGenerator2!.GetGuidAndNowTicks());
+
+    var consoleWriter = serviceProvider.GetService<IConsoleWriter>();
+    consoleWriter!.WriteSomeSpecificInfo();
+    Console.WriteLine();
+}
+
+{
+    Console.WriteLine("Only DateTimeProvider is a singleton, \nticks have to be the same, but GUIDs - nope");
+    var services = new ServiceCollection();
+
+    services.AddTransient<IGuidGenerator, GuidGenerator>();
+    services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+    services.AddTransient<IConsoleWriter, ConsoleWriter>();
+
+    var serviceProvider = services.BuildServiceProvider();
+
+    var guidGenerator = serviceProvider.GetService<IGuidGenerator>();
+    var guidGenerator2 = serviceProvider.GetService<IGuidGenerator>();
+    Console.WriteLine(guidGenerator!.GetGuidAndNowTicks());
+    Console.WriteLine(guidGenerator2!.GetGuidAndNowTicks());
+
+    var consoleWriter = serviceProvider.GetService<IConsoleWriter>();
+    consoleWriter!.WriteSomeSpecificInfo();
+    Console.WriteLine();
+}
+
+{
+    Console.WriteLine("Only GuidGenerator is a singleton, \nDateTimeProvider will be \"converted\" from transient to singleton because of that");
+    var services = new ServiceCollection();
+
+    services.AddSingleton<IGuidGenerator, GuidGenerator>();
+    services.AddTransient<IDateTimeProvider, DateTimeProvider>();
+    services.AddTransient<IConsoleWriter, ConsoleWriter>();
+
+    var serviceProvider = services.BuildServiceProvider();
+
+    var guidGenerator = serviceProvider.GetService<IGuidGenerator>();
+    var guidGenerator2 = serviceProvider.GetService<IGuidGenerator>();
+    Console.WriteLine(guidGenerator!.GetGuidAndNowTicks());
+    Console.WriteLine(guidGenerator2!.GetGuidAndNowTicks());
+
+    var consoleWriter = serviceProvider.GetService<IConsoleWriter>();
+    consoleWriter!.WriteSomeSpecificInfo();
+    Console.WriteLine();
+}
